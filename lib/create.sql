576d1592c3139ae51b70910498662daa2f8d0f2b
jdhofmann <justindhofmann@gmail.com>
2020-09-01 18:01:38 -0400

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+create table bears (
+  id INTEGER primary key,
+  name text,
+  age integer,
+  gender text,
+  color text, 
+  temperament text, 
+  alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears values(1,'Mr Chocolate', 54, 'Male', 'Brown', 'Tired',1);
+insert into bears values(2,'Rowdy', 4, 'Male', 'Black', 'Rowdy',0);
+insert into bears values(3,'Tabitha', 13, 'Female', 'Brown', 'Sexy',1);
+insert into bears values(4,'Sergeant Brown', 19, 'Female', 'Brown', 'Stoic',1);
+insert into bears values(5,'Melissa', 22, 'Female', 'Black', 'goofy',1);
+insert into bears values(6,'Grinch', 63, 'Male', 'Black', 'Old',0);
+insert into bears values(7,'Wendy', 32, 'Female', 'Black', 'Momesque',1);
+insert into bears values(8,null, 10, 'Female', 'Brown', 'Weird',1);
